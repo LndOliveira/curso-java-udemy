cc91c0041a9f73f4aa7b62b9855ad58d74c35379
Leonardo de Oliveira <leonardo.oliveira1101@hotmail.com>
2020-09-09 17:33:31 -0300

    Laço for each

---
+++ sc10_laco-for-each/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String[] { "Maria", "Bob", "Alex"};
+		
+		for (int i=0; i<vect.length; i++) {
+			System.out.println(vect[i]);
+		}
+		
+		System.out.println("--------------------------------------------");
+
+		// Laço For Each
+		
+		for (String obj : vect) {
+			System.out.println(obj);
+		}
+		
+	}
+
+}
