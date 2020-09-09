074f135625122a35dc6de8abba0333375afb5fee
Leonardo de Oliveira <leonardo.oliveira1101@hotmail.com>
2020-09-09 17:07:24 -0300

    Estruturas condicionais IF e ELSE

---
+++ s5_estrutura-condicional-if-else/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int horas;
+		
+		System.out.println("Quantas horas? ");
+		horas = sc.nextInt();
+		
+		if (horas < 12) {
+			System.out.println("Bom Dia");
+		}
+		
+		else if (horas < 18) {
+			System.out.println("Boa Tarde");
+			}
+			
+			else {
+				System.out.println("Boa noite");
+			}
+		
+		sc.close();
+		
+	}
+
+}
