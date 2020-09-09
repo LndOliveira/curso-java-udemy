e0426096007a922ecc012c20fa5401dee6326ae4
Leonardo de Oliveira <leonardo.oliveira1101@hotmail.com>
2020-09-09 17:23:09 -0300

    Exercício 3 da seção 8

---
+++ s8_exercicio3/src/application/Main.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Aluno;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Aluno aluno = new Aluno();
+		
+		aluno.nome = sc.nextLine();
+		aluno.nota1 = sc.nextDouble();
+		aluno.nota2 = sc.nextDouble();
+		aluno.nota3 = sc.nextDouble();
+		
+		System.out.printf("Nota final: %.2f%n", aluno.notaFinal());
+		
+		if (aluno.notaFinal() < 60) {
+			System.out.println("REPROVADO");
+			System.out.printf("Faltou %.2f para você ser aprovado!", aluno.ausenciaDePontos());
+		}
+		
+		else {
+			System.out.println("APROVADO");
+		}
+		
+		sc.close();
+
+	}
+
+}
---
+++ s8_exercicio3/src/entities/Aluno.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Aluno {
+
+	public String nome;
+	public double nota1, nota2, nota3;
+
+	public double notaFinal() {
+		return nota1 + nota2 + nota3;
+	}
+
+	public double ausenciaDePontos() {
+		
+		if (notaFinal() < 60.0) {
+			return 60.0 - notaFinal();
+		} 
+		
+		else {
+			return 0.0;
+		}
+	}
+
+}
